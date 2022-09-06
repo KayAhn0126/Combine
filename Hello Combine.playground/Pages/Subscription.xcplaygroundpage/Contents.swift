d9777cf2aad89ab53482777e4ba9574d6fe8dcac
KayAhn0126 <dkseotn123@gmail.com>
2022-09-06 23:51:35 +0900

    refactor: Subscription 공부 및 정리

--- Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
@@ -3,10 +3,20 @@
 import Foundation
 import Combine
 
-
-
+let subject = PassthroughSubject<String, Never>()
 
 // The print() operator prints you all lifecycle events
 
+let subscription = subject
+    .print("[Debug]")
+    .sink { value in
+    print("Subscriber received value: \(value)")
+}
 
+subject.send("Hello")
+subject.send("Hello again!")
+subject.send("Hello last ime!")
+subject.send(completion: .finished) // 퍼블리셔가 종료하는것
+subscription.cancel() // 구독 취소
+subject.send("Hello")
 //: [Next](@next)
