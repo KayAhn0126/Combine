13cc8c977d8c2e200a1a0b7fe19fb796d3c208b3
KayAhn0126 <dkseotn123@gmail.com>
2022-09-09 23:35:43 +0900

    refactor: NotificationCenter를 퍼블리셔로 활용하는 법 정리

--- Hello Combine.playground/Pages/Foundation and Combine.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Foundation and Combine.xcplaygroundpage/Contents.swift
@@ -4,17 +4,18 @@ import Foundation
 import Combine
 import UIKit
 
-
-
 // URLSessionTask Publisher and JSON Decoding Operator
 
 
-
-
 // Notifications
+let center = NotificationCenter.default
+let identifier = Notification.Name("MyNoti")
+let notiPublisher = center.publisher(for: identifier, object: nil)
+let subscription = notiPublisher.sink { _ in
+    print("Noti Received")
+}
 
-
-
+center.post(name: identifier, object: nil)
 
 
 // KeyPath binding to NSObject instances
