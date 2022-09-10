32b1cd9a0848112eaac1a9c81b81a5e7267b31ed
KayAhn0126 <dkseotn123@gmail.com>
2022-09-10 15:29:14 +0900

    refactor: Timer 퍼블리셔 정리

--- Hello Combine.playground/Pages/Foundation and Combine.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Foundation and Combine.xcplaygroundpage/Contents.swift
@@ -24,6 +24,21 @@ center.post(name: identifier, object: nil)
 
 // Timer
 // autoconnect 를 이용하면 subscribe 되면 바로 시작함
+let timerPublisher = Timer
+    .publish(every: 1, on: .main, in: .common)  // 1초마다 메인스레드에서 보통 방법으로 데이터를 방출하는 퍼블리셔를 만든다. (Timer.publish 메서드로 퍼블리셔를 만들면 Timer.TimerPublisher 타입으로 생성되고 Timer.TimerPublisher은 ConnectablePublisher 프로토콜을 채택하고있다.
+    .autoconnect()
 
+// ConnectablePublisher(Zedd0202 블로그 참고 후 작성함)
+// elements를 생성하기 전, 추가 세팅을 해야하는 경우 ConnectablePublisher를 사용. 이 Publisher는 connect()를 호출할 때 까지 element를 생성하지 않는다.
+// ConnectablePublisher 프로토콜을 채택한 퍼블리셔는 connect()를 호출해줘야 한다.
+
+
+let timerSubscription = timerPublisher.sink { time in
+    print("time: \(time)")
+}
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+    timerSubscription.cancel()
+}
 
 //: [Next](@next)
