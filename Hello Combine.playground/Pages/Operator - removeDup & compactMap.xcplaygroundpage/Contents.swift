38370ff686470b14f32b75f92f72f07a45cbd7dc
KayAhn0126 <dkseotn123@gmail.com>
2022-09-14 23:03:09 +0900

    refactor: removeDuplicates,compactMap 정리

--- Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
@@ -5,18 +5,38 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-// removeDuplicates
+// removeDuplicates -> 중복된 데이터를 어떻게 지울까?
+let words = "hey hey there! Mr Mr ?"
+    .components(separatedBy: " ")
+    .publisher
 
+words.removeDuplicates().sink { value in
+    print("Received: \(value)")
+}
 
-// compactMap
+// compactMap -> transform
 
+let strings = ["a", "1.25", "3", "def", "45", "0.23"].publisher
 
+strings.compactMap { Float($0) }.sink { value in
+    print("Received: \(value)")
+}
 
 // ignoreOutput
 
+let numbers = (1...10000).publisher
+
+numbers.ignoreOutput()
+    .sink { print("Completed with: \($0)")} receiveValue: { print($0) }
 
 
 // prefix
+
+let tens = (1...10).publisher
+
+tens
+    .prefix(3)
+    .sink { print("Completed with: \($0)") } receiveValue: { print(($0)) }
 
 
 
